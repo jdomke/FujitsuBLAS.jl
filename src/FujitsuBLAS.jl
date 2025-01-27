00d22a9324e023be632f0fd29d41408f3eda9469
jdomke <28772296+jdomke@users.noreply.github.com>
2025-01-27 21:28:58 +0900

    Update FujitsuBLAS.jl

--- src/FujitsuBLAS.jl
+++ src/FujitsuBLAS.jl
@@ -13,8 +13,8 @@ function __init__()
             # Fujitsu BLAS and also initialises the OpenMP runtime, so that we
             # can run multi-threaded BLAS calls.
             dlopen(_libdummy)
-            libs = filter(contains("libfjlapackexsve_ilp64"), dllist())
-            libfjlapackexsve_ilp64 = isone(length(libs)) ? only(libs) : ""
+            libs = filter(contains("libfjlapackexsve"), dllist())
+            libfjlapackexsve = isone(length(libs)) ? only(libs) : ""
         else
             # If the dummy library doesn't exist because, for example,
             # compilation failed, try to load the individual libraries we need
@@ -34,10 +34,10 @@ function __init__()
             isempty(libfjomp) && return Cint(0)
             dlopen(libfjomp, RTLD_GLOBAL)
             # And finally, Fujitsu BLAS
-            libfjlapackexsve_ilp64 = find_library("libfjlapackexsve_ilp64.so", library_path)
+            libfjlapackexsve = find_library("libfjlapackexsve.so", library_path)
         end
-        isempty(libfjlapackexsve_ilp64) && return Cint(0)
-        return BLAS.lbt_forward(libfjlapackexsve_ilp64; clear=true)
+        isempty(libfjlapackexsve) && return Cint(0)
+        return BLAS.lbt_forward(libfjlapackexsve; clear=true)
     end
     return Cint(0)
 end

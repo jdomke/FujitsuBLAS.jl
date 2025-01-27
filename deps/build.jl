790951b8f10481aa7b5f3d8bc55d316d87fa8db0
jdomke <28772296+jdomke@users.noreply.github.com>
2025-01-27 21:49:20 +0900

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,12 @@ using Scratch
 
 const _libdummy = joinpath(get_scratch!(UUID("32aa7d10-2208-4269-9553-8a2b02007e37"), "libdummy"), "libdummy.so")
 
+println("TRYING BUILD _libdummy")
 # Build a dummy library which automatically initalises the OpenMP runtime.
 if !isnothing(Sys.which("fcc"))
+    println("with fcc")
     mkpath(dirname(_libdummy))
     run(pipeline(`fcc -Nclang -x c - -Kopenmp -shared -lfjlapackexsve_ilp64 -o $(_libdummy)`; stdin=IOBuffer("")))
+else
+    println("...NO FCC FOUND")
 end

8f7046a4587a84d93c5c6645e530512ba0235f41
jdomke <28772296+jdomke@users.noreply.github.com>
2025-01-27 22:41:57 +0900

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -8,7 +8,7 @@ println("TRYING BUILD _libdummy")
 if !isnothing(Sys.which("fcc"))
     println("with fcc")
     mkpath(dirname(_libdummy))
-    run(pipeline(`fcc -Nclang -x c - -Kopenmp -Knolargepage -shared -Wl,-rpath=\$(dirname \$(which fcc))/../lib64 -lfjlapackexsve_ilp64 -o $(_libdummy)`; stdin=IOBuffer("")))
+    run(pipeline(`fcc -Nclang -x c - -Kopenmp -Knolargepage -shared -Wl,-rpath="\$(dirname "\$(which fcc)")/../lib64" -lfjlapackexsve_ilp64 -o $(_libdummy)`; stdin=IOBuffer("")))
 else
     println("...NO FCC FOUND")
 end
